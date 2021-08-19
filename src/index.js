ba0b6ed4e337c888bf0b3ae74dfbf29474f3217a
Rexxar1919 <vladimir.popov1998@mail.ru>
2021-08-19 11:37:25 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n), reverse = "";
+    console.log(str);
+    for (let biba = str.length - 1; biba >= 0; biba--) {
+        reverse += str[biba];
+    }
+    // let reverse = str.split('').reverse().join('');
+    // let newN = parseInt(reverse, 10);
+    return parseInt(reverse, 10);
 }
